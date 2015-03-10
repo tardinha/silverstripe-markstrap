317f432cd053919454d122591e092ddab8ea4c74
Mark Taylor <tardinha@gmail.com>
2015-03-10 14:27:08 +1100

    Added codemagic

--- _config.php
+++ _config.php
@@ -8,3 +8,21 @@ LeftAndMain::require_javascript(basename(__DIR__) . '/javascript/markstrap.js');
 
 // Set the site locale
 i18n::set_locale('en_AU');
+
+// CMS Batch Actions
+CMSBatchActionHandler::register('hidefrommenus', 'CMSBatchAction_HideFromMenus');
+CMSBatchActionHandler::register('hidefromsearch', 'CMSBatchAction_HideFromSearch');
+
+// Visualblocks plugin
+HtmlEditorConfig::get('cms')->enablePlugins(array('visualblocks' => '../../../framework/thirdparty/tinymce/plugins/visualblocks/editor_plugin.js'));
+HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'visualblocks');
+
+// Remove formatting button
+HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'removeformat');
+
+// Codemagic plugin for TinyMCE
+HtmlEditorConfig::get('cms')->enablePlugins(array(
+	'codemagic' => sprintf('../../../markstrap/thirdparty/codemagic/editor_plugin_src.js')
+));
+HtmlEditorConfig::get('cms')->insertButtonsBefore('fullscreen', 'codemagic');
+HtmlEditorConfig::get('cms')->removeButtons('code');
